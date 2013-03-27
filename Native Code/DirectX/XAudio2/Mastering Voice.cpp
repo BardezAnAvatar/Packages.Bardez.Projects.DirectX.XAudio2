0f25823143f990288ad7b304d949c91316a30a62
Erik "Bardez" Frantz <bardez@bardez.net>
2013-03-27 02:56:59 +0000

    Refactored Voice implementation such that Voice is no longer instantiatable; removed duplicate code; un-implemented inappropriate functions for Mastering Voice based off of MSDN documentation
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C758

--- Native Code/DirectX/XAudio2/Mastering Voice.cpp
+++ Native Code/DirectX/XAudio2/Mastering Voice.cpp
@@ -37,6 +37,7 @@ MasteringVoice::MasteringVoice(IXAudio2MasteringVoice* pointer)
 MasteringVoice::~MasteringVoice()
 {
 	this->DisposeUnmanaged();
+	GC::SuppressFinalize(this);
 }
 
 /// <summary>Destructor</summary>
@@ -45,54 +46,11 @@ MasteringVoice::!MasteringVoice()
 {
 	this->DisposeUnmanaged();
 }
-
-/// <summary>Destructor logic, disposes the object</summary>
-void MasteringVoice::DisposeUnmanaged()
-{
-	// Dispose this voice
-	if (this->XAudio2MasteringVoice)
-	{
-		this->XAudio2MasteringVoice->DestroyVoice();
-		this->XAudio2MasteringVoice = NULL;
-	}
-}
 #pragma endregion
 
 
 	
 #pragma region Methods
-/// <summary>Destroys the voice.</summary>
-/// <remarks>
-///		Sets the internal pointer to null. Delete the reference to this object after use.
-///		
-///		Stops the voice as necessary, removing it from the audio graph.
-///		Waits for audio thread to be idle, so may take a few ms.
-///		Violates paradigm to call from within a callback, returning XAUDIO2_E_INVALID_CALL.
-/// </remarks>
-void MasteringVoice::DestroyVoice()
-{
-	this->XAudio2MasteringVoice->DestroyVoice();
-	this->ptrVoice = IntPtr::Zero;
-}
-
-/// <summary>Disables an effect at the given index in the effect chain of the voice object.</summary>
-/// <param name="effectIndex">Index of the effect to disable</param>
-/// <param name="operationSet">Operation set of the effect (XAUDIO2_COMMIT_NOW == 0?), identifiying a batch</param>
-/// <returns>A ResultCode indicating the result of the operation</returns>
-ResultCode MasteringVoice::DisableEffect(System::UInt32 effectIndex, System::UInt32 operationSet)
-{
-	return (ResultCode)this->XAudio2MasteringVoice->DisableEffect(effectIndex, operationSet);
-}
-
-/// <summary>Enables an effect at the given index in the effect chain of the voice object.</summary>
-/// <param name="effectIndex">Index of the effect to enable</param>
-/// <param name="operationSet">Operation set of the effect (XAUDIO2_COMMIT_NOW == 0?), identifiying a batch</param>
-/// <returns>A ResultCode indicating the result of the operation</returns>
-ResultCode MasteringVoice::EnableEffect(System::UInt32 effectIndex, System::UInt32 operationSet)
-{
-	return (ResultCode)this->XAudio2MasteringVoice->EnableEffect(effectIndex, operationSet);
-}
-
 /// <summary>Gets the volume levels of this voice, per channel</summary>
 /// <param name="channels">Count of the voice's channels</param>
 /// <returns>An array of Single objects depicting </returns>
@@ -107,63 +65,7 @@ ResultCode MasteringVoice::EnableEffect(System::UInt32 effectIndex, System::UInt
 /// </remarks>
 array<System::Single>^ MasteringVoice::GetChannelVolumes(System::UInt32 channels)
 {
-	float* volumes = new float[channels];
-	array<System::Single>^ outVolumes = gcnew array<System::Single>(channels);
-
-	this->XAudio2MasteringVoice->GetChannelVolumes(channels, volumes);
-
-	for (System::UInt32 i = 0; i < channels; ++i)
-		outVolumes[i] = volumes[i];
-
-	return outVolumes;
-}
-					
-/// <summary>Gets the effect parameters of a specified effect</summary>
-/// <param name="effectIndex">Index of the effect to get the parameters of</param>
-/// <param name="parameters">Output EffectParameterBase class</param>
-/// <returns>A ResultCode indicating the result of the operation. E_NOTIMPL indicates no generic parameter control interface.</returns>
-/// <remarks>
-///		From MSDN:
-///		The data in [parameters] is completely effect-specific and determined by the implementation of the IXAPOParameters::SetParameters function.
-///		The data passed to SetParameters can be used to set the state of the XAPO and control the behavior of the IXAPO::Process function.
-/// </remarks>
-ResultCode MasteringVoice::GetEffectParameters(System::UInt32 effectIndex, [System::Runtime::InteropServices::Out] IEffectParameter^ %parameters)
-{
-	parameters = nullptr;
-	void** parameter;
-	System::UInt32* size;
-	this->effects[effectIndex]->Effect->Parameters->ToUnmanaged(parameter, size);
-
-	//This just writes to the struct pointed to, no memory allocaction
-	ResultCode result = (ResultCode)this->XAudio2MasteringVoice->GetEffectParameters(effectIndex, *parameter, *size);
-
-	if (result == ResultCode::Success_OK)
-		this->effects[effectIndex]->Effect->Parameters->RepopulateFromUnmanaged(*parameter, *size);
-
-	//deallocate the copied, unmanaged pointer
-	this->effects[effectIndex]->Effect->Parameters->ReleaseMemory(parameter);
-	delete size;
-
-	return result;
-}
-
-/// <summary>Gets the state of the specified effect</summary>
-/// <param name="effectIndex">Index of the effect to get the parameters of</param>
-/// <returns>A Boolean indicating true if enabled, false if disabled</returns>
-System::Boolean MasteringVoice::GetEffectState(System::UInt32 effectIndex)
-{
-	BOOL enabled;
-	this->XAudio2MasteringVoice->GetEffectState(effectIndex, &enabled);
-	return System::Convert::ToBoolean(enabled);
-}
-
-/// <summary>Gets the voice's parameters</summary>
-/// <returns>A FilterParameter object containing filter parameter info</returns>
-FilterParameter^ MasteringVoice::GetFilterParameters()
-{
-	XAUDIO2_FILTER_PARAMETERS parameters;
-	this->XAudio2MasteringVoice->GetFilterParameters(&parameters);
-	return gcnew FilterParameter(parameters);
+	throw gcnew InvalidOperationException("This action is not valid for mastering voices.");
 }
 
 /// <summary>Gets the voice's parameters</summary>
@@ -171,9 +73,7 @@ FilterParameter^ MasteringVoice::GetFilterParameters()
 /// <returns>A FilterParameter object containing filter parameter info</returns>
 FilterParameter^ MasteringVoice::GetOutputFilterParameters(Voice^ destination)
 {
-	XAUDIO2_FILTER_PARAMETERS parameters;
-	this->XAudio2MasteringVoice->GetOutputFilterParameters(destination->XAudio2Voice, &parameters);
-	return gcnew FilterParameter(parameters);
+	throw gcnew InvalidOperationException("This action is not valid for mastering voices.");
 }
 
 /// <summary>Gets the voice's volume output matrix for each channel</summary>
@@ -191,42 +91,7 @@ FilterParameter^ MasteringVoice::GetOutputFilterParameters(Voice^ destination)
 ///	</remarks>
 array<System::Single>^ MasteringVoice::GetOutputMatrix(Voice^ destination, System::UInt32 sourceChannels, System::UInt32 destinationChannels)
 {
-	float* matrix = new float[sourceChannels * destinationChannels];
-	this->XAudio2MasteringVoice->GetOutputMatrix(destination->XAudio2Voice, sourceChannels, destinationChannels, matrix);
-
-	array<System::Single>^ outMatrix = gcnew array<System::Single>(sourceChannels * destinationChannels);
-
-	// copy from unmanaged to managed
-	for (System::UInt32 i = 0; i < sourceChannels * destinationChannels; ++i)
-		outMatrix[i] = matrix[i];
-
-	return outMatrix;
-}
-
-/// <summary>Gets the details data of this object</summary>
-/// <returns>A VoiceDetails reference to detail data</returns>
-VoiceDetails^ MasteringVoice::GetVoiceDetails()
-{
-	XAUDIO2_VOICE_DETAILS details;
-	this->XAudio2MasteringVoice->GetVoiceDetails(&details);
-	return gcnew VoiceDetails(details);
-}
-
-/// <summary>Gets the overall volume level of this voice.</summary>
-/// <returns>
-///		A Single-precision floating point integer describing the volume.
-///		From MSDN:
-///		Volume levels are expressed as floating-point amplitude multipliers between -224 to 224, with a maximum gain of 144.5 dB.
-///		A volume level of 1 means there is no attenuation or gain and 0 means silence. Negative levels can be used to invert the audio's phase.
-///	</returns>
-/// <remarks>
-///		Always returns the volume most recently set by <see cref="SetVolume" /> This value may not yet be in effect.
-/// </remarks>
-System::Single MasteringVoice::GetVolume()
-{
-	float volume;
-	this->XAudio2MasteringVoice->GetVolume(&volume);
-	return volume;
+	throw gcnew InvalidOperationException("This action is not valid for mastering voices.");
 }
 
 /// <summary>Sets the volume of the voice for each channel</summary>
@@ -241,77 +106,9 @@ System::Single MasteringVoice::GetVolume()
 /// </remarks>
 ResultCode MasteringVoice::SetChannelVolumes(array<System::Single>^ volumes, System::UInt32 operationSet)
 {
-	float* unmanagedVol = new float[volumes->Length];
-	System::Runtime::InteropServices::Marshal::Copy(volumes, 0, IntPtr(unmanagedVol), volumes->Length);
-	return (ResultCode)this->XAudio2MasteringVoice->SetChannelVolumes(volumes->Length, unmanagedVol, operationSet);
+	throw gcnew InvalidOperationException("This action is not valid for mastering voices.");
 }
-					
-/// <summary>Replaces this Voice's effect chain.</summary>
-/// <param name="chain">Effect chain to be applied. Passing null will remove existing effects.</param>
-/// <returns>S_OK on success, otherwise an error code.</returns>
-/// <remarks>
-///		The number of output channels is constant upon creation, and the effects must honor that number.
-///		If any portion of the effect chain fails, the entire chain fails and none of it is applied.
-///		After attaching an effect, the client should no longer reference it, as XAudio2 will handle it going forward.
-/// </remarks>
-ResultCode MasteringVoice::SetEffectChain(System::Collections::Generic::List<EffectDescriptor^>^ chain)
-{
-	this->effects = chain;
-	ResultCode result;
 
-	if (this->effects == nullptr)
-		result = (ResultCode)this->XAudio2Voice->SetEffectChain(NULL);
-	else
-	{
-		XAUDIO2_EFFECT_CHAIN effectChain;
-
-		effectChain.EffectCount = this->effects->Count;
-		effectChain.pEffectDescriptors = new XAUDIO2_EFFECT_DESCRIPTOR[this->effects->Count];
-
-		//copy descriptors
-		for (System::Int32 i = 0; i < this->effects->Count; ++i)
-			effectChain.pEffectDescriptors[i] = this->effects[i]->ToUnmanaged();
-
-		result = (ResultCode)this->XAudio2MasteringVoice->SetEffectChain(&effectChain);
-	}
-
-	return result;
-}
-					
-/// <summary>Sets the parameters for  an effect in this voice's effect in the chain as specified in the index</summary>
-/// <param name="effectIndex">Index within the effect chain to set parameters for</param>
-/// <param name="parameters">EffectParameterBase class to set</param>
-/// <param name="operationSet">Operation set of the effect (XAUDIO2_COMMIT_NOW == 0?), identifiying a batch</param>
-/// <returns>S_OK on success, otherwise an error code.</returns>
-/// <remarks>
-///		From MSDN:
-///		The specific effect being used determines the valid size and format of the pParameters buffer. The call will
-///			fail if pParameters is invalid or if ParametersByteSize is not exactly the size that the effect expects.
-///			The client must take care to direct the SetEffectParameters call to the right effect. If this call is directed
-///			to a different effect that happens to accept the same parameter block size, the parameters will be interpreted
-///			differently. This may lead to unexpected results.
-///
-///		The memory pointed to by pParameters must not be freed immediately, because XAudio2 will need to refer to it later
-///			when the parameters actually are applied to the effect. This happens during the next audio processing pass if
-///			the OperationSet argument is XAUDIO2_COMMIT_NOW. Otherwise, the parameters are applied to the effect later,
-///			during the first processing pass after the IXAudio2::CommitChanges function is called with the same
-///			OperationSet argument.
-/// </remarks>
-ResultCode MasteringVoice::SetEffectParameters(System::UInt32 effectIndex, IEffectParameter^ parameters, System::UInt32 operationSet)
-{
-	void** parameter;
-	System::UInt32* size;
-	parameters->ToUnmanaged(parameter, size);
-
-	ResultCode result = (ResultCode)this->XAudio2MasteringVoice->SetEffectParameters(effectIndex, *parameter, *size);
-
-	//deallocate the pointers
-	parameters->ReleaseMemory(parameter);
-	delete size;
-
-	return result;
-}
-					
 /// <summary>Sets the voice's parameters</summary>
 /// <param name="parameter">A FilterParameter object containing filter parameter info</param>
 /// <param name="operationSet">Operation set of the effect (XAUDIO2_COMMIT_NOW == 0?), identifiying a batch</param>
@@ -322,7 +119,7 @@ ResultCode MasteringVoice::SetEffectParameters(System::UInt32 effectIndex, IEffe
 /// </remarks>
 ResultCode MasteringVoice::SetFilterParameters(FilterParameter^ parameter, System::UInt32 operationSet)
 {
-	return (ResultCode)this->XAudio2MasteringVoice->SetFilterParameters(&(parameter->ToUnmanaged()), operationSet);
+	throw gcnew InvalidOperationException("This action is not valid for mastering voices.");
 }
 
 /// <summary>Gets the voice's parameters</summary>
@@ -332,9 +129,9 @@ ResultCode MasteringVoice::SetFilterParameters(FilterParameter^ parameter, Syste
 /// <returns>S_OK on success, otherwise an error code.</returns>
 ResultCode MasteringVoice::SetOutputFilterParameters(Voice^ destination, FilterParameter^ parameter, System::UInt32 operationSet)
 {
-	return (ResultCode)this->XAudio2MasteringVoice->SetOutputFilterParameters(destination->XAudio2Voice, &(parameter->ToUnmanaged()), operationSet);
+	throw gcnew InvalidOperationException("This action is not valid for mastering voices.");
 }
-					
+
 /// <summary>Sets the voice's volume output matrix for each channel</summary>
 /// <param name="destination">Destination Voice to set the matrix for. If null and only one destination, will set for that destination Voice.</param>
 /// <param name="sourceChannels">Number of channels of the source Voice.</param>
@@ -353,10 +150,7 @@ ResultCode MasteringVoice::SetOutputFilterParameters(Voice^ destination, FilterP
 ///	</remarks>
 ResultCode MasteringVoice::SetOutputMatrix(Voice^ destination, System::UInt32 sourceChannels, System::UInt32 destinationChannels, array<System::Single>^ volumeMatrix, System::UInt32 operationSet)
 {
-	float* unmanagedVol = new float[volumeMatrix->Length];
-	System::Runtime::InteropServices::Marshal::Copy(volumeMatrix, 0, IntPtr(unmanagedVol), volumeMatrix->Length);
-
-	return (ResultCode)this->XAudio2MasteringVoice->SetOutputMatrix(destination->XAudio2Voice, sourceChannels, destinationChannels, unmanagedVol, operationSet);
+	throw gcnew InvalidOperationException("This action is not valid for mastering voices.");
 }
 
 /// <summary>Designates a new set of submix or mastering Voice destinations to receive this Voice's output</summary>
@@ -365,33 +159,7 @@ ResultCode MasteringVoice::SetOutputMatrix(Voice^ destination, System::UInt32 so
 /// <remarks>if voices is null, it is sent to the current mastering voice. To not send anywhere, set size of the array to 0.</remarks>
 ResultCode MasteringVoice::SetOutputVoices(array<VoiceSendDescriptor^>^ voices)
 {
-	XAUDIO2_VOICE_SENDS sendList;
-	sendList.SendCount = voices->Length;
-	sendList.pSends = new XAUDIO2_SEND_DESCRIPTOR[voices->Length];
-	
-	//copy
-	for (System::Int32 i = 0; i < voices->Length; ++i)
-		sendList.pSends[i] = voices[i]->ToUnmanaged();
-
-	return (ResultCode)this->XAudio2MasteringVoice->SetOutputVoices(&sendList);
-}
-
-/// <summary>Sets the voice's volume output matrix for each channel</summary>
-/// <param name="volume">Value of the overall volume to set for this voice</param>
-/// <param name="operationSet">Operation set of the effect (XAUDIO2_COMMIT_NOW == 0?), identifiying a batch</param>
-/// <returns>S_OK on success, otherwise an error code.</returns>
-/// <remarks>
-///		From MSDN:
-///		The master volume level is applied at different times depending on the type of voice. For submix and mastering voices the volume level is applied
-///			just before the voice's built in filter and effect chain is applied. For source voices the master volume level is applied after
-///			the voice's filter and effect chain is applied.
-///		Volume levels are expressed as floating-point amplitude multipliers between -XAUDIO2_MAX_VOLUME_LEVEL and XAUDIO2_MAX_VOLUME_LEVEL (-224 to 224),
-///			with a maximum gain of 144.5 dB. A volume level of 1.0 means there is no attenuation or gain and 0 means silence.
-///			Negative levels can be used to invert the audio's phase.
-/// </remarks>
-ResultCode MasteringVoice::SetVolume(System::Single volume, System::UInt32 operationSet)
-{
-	return (ResultCode)this->XAudio2MasteringVoice->SetVolume(volume, operationSet);
+	throw gcnew InvalidOperationException("This action is not valid for mastering voices.");
 }
 
 #if _XAUDIO2_VERSION_ >= 8
@@ -406,3 +174,4 @@ System::UInt32 MasteringVoice::GetChannelMask()
 }
 #endif
 #pragma endregion
+
--- Native Code/DirectX/XAudio2/Mastering Voice.h
+++ Native Code/DirectX/XAudio2/Mastering Voice.h
@@ -50,35 +50,12 @@ namespace Bardez
 					/// <summary>Destructor</summary>
 					/// <remarks>Finalize()</remarks>
 					!MasteringVoice();
-
-					/// <summary>Destructor logic, disposes the object</summary>
-					void DisposeUnmanaged();
 				#pragma endregion
 
 
 
 				#pragma region Methods
 				public:
-					/// <summary>Destroys the voice.</summary>
-					/// <remarks>
-					///		Stops the voice as necessary, removing it from the audio graph.
-					///		Waits for audio thread to be idle, so may take a few ms.
-					///		Violates paradigm to call from within a callback, returning XAUDIO2_E_INVALID_CALL.
-					/// </remarks>
-					virtual void DestroyVoice() override;
-
-					/// <summary>Disables an effect at the given index in the effect chain of the voice object.</summary>
-					/// <param name="effectIndex">Index of the effect to disable</param>
-					/// <param name="operationSet">Operation set of the effect (XAUDIO2_COMMIT_NOW == 0?), identifiying a batch</param>
-					/// <returns>A ResultCode indicating the result of the operation</returns>
-					virtual ResultCode DisableEffect(System::UInt32 effectIndex, System::UInt32 operationSet) override;
-
-					/// <summary>Enables an effect at the given index in the effect chain of the voice object.</summary>
-					/// <param name="effectIndex">Index of the effect to enable</param>
-					/// <param name="operationSet">Operation set of the effect (XAUDIO2_COMMIT_NOW == 0?), identifiying a batch</param>
-					/// <returns>A ResultCode indicating the result of the operation</returns>
-					virtual ResultCode EnableEffect(System::UInt32 effectIndex, System::UInt32 operationSet) override;
-
 					/// <summary>Gets the volume levels of this voice, per channel</summary>
 					/// <param name="channels">Count of the voice's channels</param>
 					/// <returns>An array of Single objects depicting </returns>
@@ -92,26 +69,6 @@ namespace Bardez
 					///		A volume of 1 means there is no attenuation or gain, 0 means silence, and negative levels can be used to invert the audio's phase.
 					/// </remarks>
 					virtual array<System::Single>^ GetChannelVolumes(System::UInt32 channels) override;
-
-					/// <summary>Gets the effect parameters of a specified effect</summary>
-					/// <param name="effectIndex">Index of the effect to get the parameters of</param>
-					/// <param name="parameters">Output EffectParameterBase class</param>
-					/// <returns>A ResultCode indicating the result of the operation. E_NOTIMPL indicates no generic parameter control interface.</returns>
-					/// <remarks>
-					///		From MSDN:
-					///		The data in [parameters] is completely effect-specific and determined by the implementation of the IXAPOParameters::SetParameters function.
-					///		The data passed to SetParameters can be used to set the state of the XAPO and control the behavior of the IXAPO::Process function.
-					/// </remarks>
-					virtual ResultCode GetEffectParameters(System::UInt32 effectIndex, [System::Runtime::InteropServices::Out] IEffectParameter^ %parameters) override;
-
-					/// <summary>Gets the state of the specified effect</summary>
-					/// <param name="effectIndex">Index of the effect to get the parameters of</param>
-					/// <returns>A Boolean indicating true if enabled, false if disabled</returns>
-					virtual System::Boolean GetEffectState(System::UInt32 effectIndex) override;
-
-					/// <summary>Gets the voice's parameters</summary>
-					/// <returns>A FilterParameter object containing filter parameter info</returns>
-					virtual FilterParameter^ GetFilterParameters() override;
 
 					/// <summary>Gets the voice's parameters</summary>
 					/// <param name="destination">Destination Voice the filter will output to</param>
@@ -133,22 +90,6 @@ namespace Bardez
 					///	</remarks>
 					virtual array<System::Single>^ GetOutputMatrix(Voice^ destination, System::UInt32 sourceChannels, System::UInt32 destinationChannels) override;
 
-					/// <summary>Gets the details data of this object</summary>
-					/// <returns>A VoiceDetails reference to detail data</returns>
-					virtual VoiceDetails^ GetVoiceDetails() override;
-
-					/// <summary>Gets the overall volume level of this voice.</summary>
-					/// <returns>
-					///		A Single-precision floating point integer describing the volume.
-					///		From MSDN:
-					///		Volume levels are expressed as floating-point amplitude multipliers between -224 to 224, with a maximum gain of 144.5 dB.
-					///		A volume level of 1 means there is no attenuation or gain and 0 means silence. Negative levels can be used to invert the audio's phase.
-					///	</returns>
-					/// <remarks>
-					///		Always returns the volume most recently set by <see cref="SetVolume" /> This value may not yet be in effect.
-					/// </remarks>
-					virtual System::Single GetVolume() override;
-
 					/// <summary>Sets the volume of the voice for each channel</summary>
 					/// <param name="volumes">Array of volumes for the voice.</param>
 					/// <param name="operationSet">Operation set to be applied.</param>
@@ -160,37 +101,6 @@ namespace Bardez
 					///		A volume level of 1 means there is no attenuation or gain and 0 means silence. Negative levels can be used to invert the audio's phase.
 					/// </remarks>
 					virtual ResultCode SetChannelVolumes(array<System::Single>^ volumes, System::UInt32 operationSet) override;
-
-					/// <summary>Replaces this Voice's effect chain.</summary>
-					/// <param name="chain">Effect chain to be applied. Passing null will remove existing effects.</param>
-					/// <returns>S_OK on success, otherwise an error code.</returns>
-					/// <remarks>
-					///		The number of output channels is constant upon creation, and the effects must honor that number.
-					///		If any portion of the effect chain fails, the entire chain fails and none of it is applied.
-					///		After attaching an effect, the client should no longer reference it, as XAudio2 will handle it going forward.
-					/// </remarks>
-					virtual ResultCode SetEffectChain(System::Collections::Generic::List<EffectDescriptor^>^ chain) override;
-
-					/// <summary>Sets the parameters for  an effect in this voice's effect in the chain as specified in the index</summary>
-					/// <param name="effectIndex">Index within the effect chain to set parameters for</param>
-					/// <param name="parameters">Object array of parameters to set</param>
-					/// <param name="operationSet">Operation set of the effect (XAUDIO2_COMMIT_NOW == 0?), identifiying a batch</param>
-					/// <returns>S_OK on success, otherwise an error code.</returns>
-					/// <remarks>
-					///		From MSDN:
-					///		The specific effect being used determines the valid size and format of the pParameters buffer. The call will
-					///			fail if pParameters is invalid or if ParametersByteSize is not exactly the size that the effect expects.
-					///			The client must take care to direct the SetEffectParameters call to the right effect. If this call is directed
-					///			to a different effect that happens to accept the same parameter block size, the parameters will be interpreted
-					///			differently. This may lead to unexpected results.
-					///
-					///		The memory pointed to by pParameters must not be freed immediately, because XAudio2 will need to refer to it later
-					///			when the parameters actually are applied to the effect. This happens during the next audio processing pass if
-					///			the OperationSet argument is XAUDIO2_COMMIT_NOW. Otherwise, the parameters are applied to the effect later,
-					///			during the first processing pass after the IXAudio2::CommitChanges function is called with the same
-					///			OperationSet argument.
-					/// </remarks>
-					virtual ResultCode SetEffectParameters(System::UInt32 effectIndex, IEffectParameter^ parameters, System::UInt32 operationSet) override;
 
 					/// <summary>Sets the voice's parameters</summary>
 					/// <param name="parameter">A FilterParameter object containing filter parameter info</param>
@@ -232,21 +142,6 @@ namespace Bardez
 					/// <returns>S_OK on success, otherwise an error code.</returns>
 					/// <remarks>if voices is null, it is sent to the current mastering voice. To not send anywhere, set size of the array to 0.</remarks>
 					virtual ResultCode SetOutputVoices(array<VoiceSendDescriptor^>^ voices) override;
-
-					/// <summary>Sets the voice's volume output matrix for each channel</summary>
-					/// <param name="volume">Value of the oerall volume to set for this voice</param>
-					/// <param name="operationSet">Operation set of the effect (XAUDIO2_COMMIT_NOW == 0?), identifiying a batch</param>
-					/// <returns>S_OK on success, otherwise an error code.</returns>
-					/// <remarks>
-					///		From MSDN:
-					///		The master volume level is applied at different times depending on the type of voice. For submix and mastering voices the volume level is applied
-					///			just before the voice's built in filter and effect chain is applied. For source voices the master volume level is applied after
-					///			the voice's filter and effect chain is applied.
-					///		Volume levels are expressed as floating-point amplitude multipliers between -XAUDIO2_MAX_VOLUME_LEVEL and XAUDIO2_MAX_VOLUME_LEVEL (-224 to 224),
-					///			with a maximum gain of 144.5 dB. A volume level of 1.0 means there is no attenuation or gain and 0 means silence.
-					///			Negative levels can be used to invert the audio's phase.
-					/// </remarks>
-					virtual ResultCode SetVolume(System::Single volume, System::UInt32 operationSet) override;
 
 #if _XAUDIO2_VERSION_ >= 8
 					/// <summary>Gets this voice's channel mask</summary>
