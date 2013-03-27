2251d2ade6ba148c7502ab2f3c01eb37c0d4cf5c
Erik "Bardez" Frantz <bardez@bardez.net>
2013-03-27 03:03:16 +0000

    Compleltely changed the Effect base into and interface
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C766

---
+++ Native Code/DirectX/XAudio2/IEffectBase.h
@@ -0,0 +1,50 @@
+
+#ifndef Bardez_Projects_Directx_XAPO_EffectBase
+#define Bardez_Projects_Directx_XAPO_EffectBase
+
+
+#include <windows.h>
+#include <xaudio2.h>
+#include "IEffectParameter.h"
+
+
+using namespace System;
+
+
+namespace Bardez
+{
+	namespace Projects
+	{
+		namespace DirectX
+		{
+			namespace XAudio2
+			{
+				namespace XAPO
+				{
+					/// <summary>Interface for defining XAPO audio effect classes</summary>
+					private interface class IEffectBase : public IDisposable
+					{
+					#pragma region Properties
+					public:
+						/// <summary>Unmanaged pointer to the effect COM object/interface</summary>
+						property IUnknown* UnmanagedPointer
+						{
+							IUnknown* get();
+							void set(IUnknown* value);
+						}
+					#pragma endregion
+
+
+
+					#pragma region Methods
+					public:
+						/// <summary>Returns an empty instance of the related parameter type</summary>
+						IEffectParameter^ CreateParameters();
+					#pragma endregion
+					};
+				}
+			}
+		}
+	}
+}
+#endif
